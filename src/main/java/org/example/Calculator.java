0cb230a8a4d735aede558ae5eb22946165b525ef
Rohitangshu Bose <rbose2002@gmail.com>
2026-03-15 12:04:16 +0530

    Checking error flow

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -16,9 +16,9 @@ public class Calculator {
     }
 
     public static double divide(double num1, double num2){
-        if(num2 == 0){
-            throw new ArithmeticException("Division by zero");
-        }
+//        if(num2 == 0){
+//            throw new ArithmeticException("Division by zero");
+//        }
         return num1 / num2;
     }
 
